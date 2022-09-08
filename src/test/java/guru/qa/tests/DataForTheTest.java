a0752f31f38730b65db342e3e5c43de7f82129d6
Loarlam <Loarlam@ya.ru>
2022-09-08 18:09:04 +0300

    Добавлена потерянная буква в слове "вакансии"

--- src/test/java/guru/qa/tests/DataForTheTest.java
+++ src/test/java/guru/qa/tests/DataForTheTest.java
@@ -1,7 +1,7 @@
 package guru.qa.tests;
 
 public class DataForTheTest {
-    String h1Text = "Ваканси Tele2",
+    String h1Text = "Вакансии Tele2",
             additionalPath = "/about/career/vacancies",
             jobTitle = "Руководитель",
             errorJobTitle = "ЖЖЖЖ";
